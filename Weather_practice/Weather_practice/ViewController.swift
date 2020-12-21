90c98144ae97640043e28144b869992c47be0a5f
hebaekkk <hebaek@big-hand.kr>
2020-12-21 11:21:36 +0900

    before URL Process

--- Weather_practice/Weather_practice/ViewController.swift
+++ Weather_practice/Weather_practice/ViewController.swift
@@ -6,14 +6,89 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+//Location : CoreLocation
+//Table View
+//Custom Cell: CollectionView
+//API / requset to get the data
 
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
+
+    @IBOutlet var table: UITableView!
+    var models = [Weather]()
+    
+    let locationManager = CLLocationManager()
+    
+    var currentLocation: CLLocation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Register 2 cells
+        table.register(HourlyTableViewCell.nib(), forCellReuseIdentifier: HourlyTableViewCell.identifier)
+        table.register(WeatherTableViewCell.nib(), forCellReuseIdentifier: WeatherTableViewCell.identifier)
+        
+        
+    
+        table.delegate = self
+        table.dataSource = self
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupLocation()
+
+    }
+    //Location
+    func setupLocation() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingHeading()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if !locations.isEmpty, currentLocation == nil {
+            currentLocation = locations.first
+            locationManager.stopUpdatingLocation()
+            requestWeatherForLocation()
+        }
+    }
+    
+    func requestWeatherForLocation() {
+        guard let currentLocation = currentLocation else {
+            print("로케이션 설정 안됐는데?..")
+            return
+        }
+        let long = currentLocation.coordinate.longitude
+        let lat = currentLocation.coordinate.latitude
+        
+        let url = ""
+        
+        URLSession.shared.dataTask(with: URL(string: url), completionHandler: {data, response, error in
+            
+            //Validataion
+            //Convert data to models/ some object
+            
+            //Update
+        })
+        
+        print("(\(long) | \(lat)")
     }
 
+    
+    //Table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 
+}
+
+struct Weather {
+    //
 }
 
