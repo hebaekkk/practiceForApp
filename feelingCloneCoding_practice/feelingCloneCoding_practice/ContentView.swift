c7f9fa2fdd76ff83b2ce94098fa9dd704bdaa47f
hebaekkk <hebaek@big-hand.kr>
2020-12-25 01:02:29 +0900

    12.25 start

--- feelingCloneCoding_practice/feelingCloneCoding_practice/ContentView.swift
+++ feelingCloneCoding_practice/feelingCloneCoding_practice/ContentView.swift
@@ -35,8 +35,8 @@ struct Rating : View {
             Spacer(minLength: 0)
             
             Smile(value: value)
-                .stroke(Color.black, lineWidth: 3)
-                .frame(height: 150)
+                //.stroke(Color.black, lineWidth: 3)
+                //.frame(height: 150)
             
             Slider(value: $value)
                 .padding()
@@ -58,15 +58,16 @@ struct Smile: Shape {
             
             let center = rect.width / 2
             
-            let downRadius: CGFloat = 25
+            let downRadius: CGFloat = 25 * (value)
+            path.move(to: CGPoint(x: center - 150, y: 0))
             
             let to1 = CGPoint(x: center, y: downRadius)
-            let control1 = CGPoint(x: center - 145, y: 0)
-            let control2 = CGPoint(x: center - 145, y: downRadius)
+            let control1 = CGPoint(x: center - 150, y: 0)
+            let control2 = CGPoint(x: center - 150, y: downRadius)
             
             let to2 = CGPoint(x: center + 150, y: 0)
-            let control3 = CGPoint(x: center + 145, y: downRadius)
-            let control4 = CGPoint(x: center + 145, y: 0)
+            let control3 = CGPoint(x: center + 150, y: downRadius)
+            let control4 = CGPoint(x: center + 150, y: 0)
             
             path.addCurve(to: to1, control1: control1, control2: control2)
             path.addCurve(to: to2, control1: control3, control2: control4 )
